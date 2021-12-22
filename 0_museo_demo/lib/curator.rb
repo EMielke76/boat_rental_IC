809c50ea5146abaef3c7a9ab7b78078087477d9c
Seth Perna <gseth26@gmail.com>
2021-12-22 18:03:18 -0500

    itertion 3 mostly complete

--- 0_museo_demo/lib/curator.rb
+++ 0_museo_demo/lib/curator.rb
@@ -18,4 +18,21 @@ class Curator
   def find_artist_by_id(id)
     @artists.select {|artist| artist.id == id}[0]
   end
+
+  def photographs_by_artist
+    artist_photo = {}
+    @artists.map do |artist|
+      artist_photo[artist] = @photographs.select {|photo| photo.artist_id == artist.id}
+    end
+    artist_photo
+  end
+
+  def artists_with_multiple_photographs
+    photographer = photographs_by_artist.select do |key, value|
+      value.length > 1
+    end
+    photographer.keys.map do |photog|
+      photog.name
+    end
+  end
 end
