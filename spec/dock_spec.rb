c6189a68109a60f31b6916b800a0cab156b7f603
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-22 18:53:36 -0700

    Iteartion II done and dusted

---
+++ spec/dock_spec.rb
@@ -0,0 +1,36 @@
+require './lib/boat'
+require './lib/renter'
+require './lib/dock'
+
+RSpec.describe Dock do
+  let(:dock) {Dock.new("The Rowing Dock", 3)}
+
+  let(:kayak_1) {Boat.new(:kayak, 20)}
+  let(:kayak_2) {Boat.new(:kayak, 20)}
+  let(:sup_1) {Boat.new(:standup_paddle_board, 15)}
+
+  let(:patrick) {Renter.new("Patrick Star", "4242424242424242")}
+  let(:eugene) {Renter.new("Eugene Crabs", "1313131313131313")}
+
+  it 'exists' do
+    expect(dock).to be_a(Dock)
+  end
+
+  it 'has attributes' do
+    expect(dock.name).to eq("The Rowing Dock")
+    expect(dock.max_rental_time).to eq(3)
+  end
+
+  it '#rent and #rental_log' do
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    expected = {
+      kayak_1 => patrick,
+      kayak_2 => patrick,
+      sup_1 => eugene
+    }
+
+    expect(dock.rental_log).to eq(expected)
+  end
+end
